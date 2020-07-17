34c202a6a144e2620763741fe98034e0eeaa2fc9
Lucio Chavez <luciobertinchavez@gmail.com>
2020-07-17 12:21:58 -0500

    set theme objects as JSON.parse

--- src/themes/constants.ts
+++ src/themes/constants.ts
@@ -4,7 +4,7 @@ import {createMuiTheme} from '@material-ui/core/styles';
 export const LIGHT_THEME = 'lightTheme';
 export const DARK_THEME = 'darkTheme';
 
-const lightThemeObject = {
+/*const lightThemeObject = {
   palette: {
     primary: {
       main: '#43a047',
@@ -42,13 +42,13 @@ const darkThemeObject = {
         default: '#121212',
       }
     },
-  }
+  }*/
 //TODO: If you have time, move the recent updated theme objects to JSON.parse
-//const lightTheme = createMuiTheme(JSON.parse("{\n   \"palette\": {\n      \"primary\": {\n         \"main\": \"#43a047\"\n      },\n      \"secondary\": {\n         \"main\": \"#f44336\"\n      }\n   }\n}"))
-//const darkTheme = createMuiTheme(JSON.parse("{\n   \"overrides\": {\n      \"MuiCssBaseline\": {\n         \"@global\": {\n            \"body\": {\n               \"backgroundColor\": \"#424242\"\n            }\n         }\n      }\n   },\n   \"palette\": {\n      \"type\": \"dark\",\n      \"primary\": {\n         \"main\": \"#8e24aa\"\n      },\n      \"secondary\": {\n         \"main\": \"#B71C1C\"\n      },\n      \"background\": {\n         \"default\": \"#121212\"\n      }\n   }\n}"))
+const lightTheme = createMuiTheme(JSON.parse("{\n   \"palette\": {\n      \"primary\": {\n         \"main\": \"#43a047\"\n      },\n      \"secondary\": {\n         \"main\": \"#f44336\"\n      }\n   }\n}"))
+const darkTheme = createMuiTheme(JSON.parse("{\n   \"overrides\": {\n      \"MuiCssBaseline\": {\n         \"@global\": {\n            \"body\": {\n               \"backgroundColor\": \"#424242\"\n            }\n         }\n      }\n   },\n   \"palette\": {\n      \"type\": \"dark\",\n      \"primary\": {\n         \"main\": \"#8e24aa\",\n         \"light\": \"#ce93d8\"\n      },\n      \"secondary\": {\n         \"main\": \"#B71C1C\"\n      },\n      \"background\": {\n         \"default\": \"#121212\"\n      }\n   }\n}"))
 
-const lightTheme = createMuiTheme(lightThemeObject)
-const darkTheme = createMuiTheme(darkThemeObject)
+//const lightTheme = createMuiTheme(lightThemeObject)
+//const darkTheme = createMuiTheme(darkThemeObject)
   
 const themes = {
     lightTheme,
