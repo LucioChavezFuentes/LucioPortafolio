1142e3e4e2240c4c56630219e08f631d413fb983
Lucio Chavez <luciobertinchavez@gmail.com>
2020-08-18 15:04:19 -0500

    format prettier code and set aboutMeContainer style padding left and right to 0

--- src/views/ProfilePage/AbouteMe.tsx
+++ src/views/ProfilePage/AbouteMe.tsx
@@ -1,99 +1,115 @@
-import React from 'react'
+import React from "react";
 import GridContainer from "components/Grid/GridContainer";
 import GridItem from "components/Grid/GridItem";
 
 // Material IU imports
-import CustomPaper from 'components/CustomPaper/CustomPaper';
-import Typography from '@material-ui/core/Typography';
+import CustomPaper from "components/CustomPaper/CustomPaper";
+import Typography from "@material-ui/core/Typography";
 
-function AbouteMe({
-    classes, 
-    isMobile, 
-    fullWidth, 
-    gridConst: {
-        currentPixelsSpacing, 
-        currentGridSpacing
-    },
-    elevationConst: {
-        MAX_ELEVATION,
-        MEDIUM_ELEVATION,
-    },
-    intl
- }) {
-    return (
-        <div className={classes.aboutMeContainer} style={{padding: currentPixelsSpacing}}>
-              <div className='aboutMe'>
-              <GridContainer justify={isMobile ? "center" : "space-between"} alignItems='flex-start' spacing={currentGridSpacing}>
-                <GridItem  xs={fullWidth} sm={fullWidth} md={fullWidth}>
-                  <div className={classes.paperTitleContainer}>
-                    <CustomPaper elevation={MAX_ELEVATION} className="paper">
-                      <Typography color='textPrimary' className="title">
-                        {intl.formatMessage({
-                        defaultMessage: "About Me",
-                        description: 'title-about-me',
-                        id: "title-about-me",
-                        })}
-                        
-                      </Typography>
-                    </CustomPaper>
-                  </div>
-                </GridItem>
+interface Props {
+  classes: any,
+  isMobile: boolean,
+  fullWidth: number,
+  gridConst: {
+    currentPixelsSpacing: number,
+    currentGridSpacing: number
+  },
+  elevationConst: {
+    MAX_ELEVATION: number,
+    MEDIUM_ELEVATION: number,
+  },
 
-                <GridItem xs={fullWidth} sm={fullWidth} md={fullWidth}>
-                  <div className='miniTitle'>
-                    <Typography color='textPrimary' className="title">
-                      {intl.formatMessage({
-                      defaultMessage: "Philosophy",
-                      description: 'title-philosophy',
-                      id: "title-philosophy",
-                      })}
-                      
-                    </Typography>
-                  </div>
-                </GridItem>
+  intl: any
+}
 
-                <GridItem className='gridItemDescription' xs={fullWidth} sm={fullWidth} md={fullWidth}>
-                <CustomPaper elevation={MEDIUM_ELEVATION} className='paperDescription'>
-                  <p>
+const AbouteMe : React.FC<Props> = ({
+  classes,
+  isMobile,
+  fullWidth,
+  gridConst: {
+    currentPixelsSpacing,
+    currentGridSpacing
+  },
+  elevationConst: {
+    MAX_ELEVATION,
+    MEDIUM_ELEVATION,
+  },
+  intl
+} : Props) => {
+  return (
+    <div className={classes.aboutMeContainer} style={{ padding: `${currentPixelsSpacing}px 0px` }}>
+      <div className='aboutMe'>
+        <GridContainer justify={isMobile ? "center" : "space-between"} alignItems='flex-start' spacing={currentGridSpacing}>
+          <GridItem xs={fullWidth} sm={fullWidth} md={fullWidth}>
+            <div className={classes.paperTitleContainer}>
+              <CustomPaper elevation={MAX_ELEVATION} className="paper">
+                <Typography color='textPrimary' className="title">
                   {intl.formatMessage({
-                    defaultMessage: "I believe in always keep learning new things, code with the best known practices and looking forward for those yet to discover. I also trust in making good and honest work from the beginning in order to avoid extra effort and costs in the future.",
-                    description: 'philosophy',
-                    id: "philosophy",
+                    defaultMessage: "About Me",
+                    description: 'title-about-me',
+                    id: "title-about-me",
                   })}
-                  </p>
-                </CustomPaper>
-                </GridItem>
 
-                
-                  <GridItem xs={fullWidth} sm={fullWidth} md={fullWidth}>
-                    <div className='miniTitle'>
-                      <Typography color='textPrimary' className="title">
-                        {intl.formatMessage({
-                        defaultMessage: "Hobbies and Lecture",
-                        description: 'title-hobbies',
-                        id: "title-hobbies",
-                        })}
-                        
-                      </Typography>
-                    </div>
-                  </GridItem>
-
-                  <GridItem className='gridItemDescription' xs={fullWidth} sm={fullWidth} md={fullWidth}>
-                    <CustomPaper elevation={MEDIUM_ELEVATION} className='paperDescription'>
-                      <p className='hobbiesDescription'>
-                        {intl.formatMessage({
-                          defaultMessage: "Usually, I found myself playing video games, watching YouTube cartoons and reading non-fiction books for my professional growth. My last two favorite are: Code Complete of Steve McConell and Essentialism: The Disciplined Pursuit of Less of Greg McKeown.",
-                          description: 'hobbies',
-                          id: "hobbies",
-                        })}
-                      </p>
-                    </CustomPaper>
-                  </GridItem>
-
-              </GridContainer>
-              </div>
+                </Typography>
+              </CustomPaper>
             </div>
-    )
+          </GridItem>
+
+          <GridItem xs={fullWidth} sm={fullWidth} md={fullWidth}>
+            <div className='miniTitle'>
+              <Typography color='textPrimary' className="title">
+                {intl.formatMessage({
+                  defaultMessage: "Philosophy",
+                  description: 'title-philosophy',
+                  id: "title-philosophy",
+                })}
+
+              </Typography>
+            </div>
+          </GridItem>
+
+          <GridItem className='gridItemDescription' xs={fullWidth} sm={fullWidth} md={fullWidth}>
+            <CustomPaper elevation={MEDIUM_ELEVATION} className='paperDescription'>
+              <p>
+                {intl.formatMessage({
+                  defaultMessage: "I believe in always keep learning new things, code with the best known practices and looking forward for those yet to discover. I also trust in making good and honest work from the beginning in order to avoid extra effort and costs in the future.",
+                  description: 'philosophy',
+                  id: "philosophy",
+                })}
+              </p>
+            </CustomPaper>
+          </GridItem>
+
+
+          <GridItem xs={fullWidth} sm={fullWidth} md={fullWidth}>
+            <div className='miniTitle'>
+              <Typography color='textPrimary' className="title">
+                {intl.formatMessage({
+                  defaultMessage: "Hobbies and Lecture",
+                  description: 'title-hobbies',
+                  id: "title-hobbies",
+                })}
+
+              </Typography>
+            </div>
+          </GridItem>
+
+          <GridItem className='gridItemDescription' xs={fullWidth} sm={fullWidth} md={fullWidth}>
+            <CustomPaper elevation={MEDIUM_ELEVATION} className='paperDescription'>
+              <p className='hobbiesDescription'>
+                {intl.formatMessage({
+                  defaultMessage: "Usually, I found myself playing video games, watching YouTube cartoons and reading non-fiction books for my professional growth. My last two favorite are: Code Complete of Steve McConell and Essentialism: The Disciplined Pursuit of Less of Greg McKeown.",
+                  description: 'hobbies',
+                  id: "hobbies",
+                })}
+              </p>
+            </CustomPaper>
+          </GridItem>
+
+        </GridContainer>
+      </div>
+    </div>
+  )
 }
 
 export default AbouteMe
