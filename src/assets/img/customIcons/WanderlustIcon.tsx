60952a1bc1a0372abae7b0d30fa9c2b91d69d58c
Lucio Chavez <luciobertinchavez@gmail.com>
2020-09-02 13:10:30 -0500

    create WanderlustIcon component

---
+++ src/assets/img/customIcons/WanderlustIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// Custom Icons
+import {ReactComponent as WanderlustSVG} from "assets/img/logos/wanderlust-logo.svg";
+// Material IU Icon Warapper
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const WanderlustIcon = () => <SvgIcon viewBox='0 0 39 41' component={WanderlustSVG} />
+
+export default WanderlustIcon;
