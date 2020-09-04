5670e96254a22b2005731d091bb626b39d401d49
Lucio Chavez <luciobertinchavez@gmail.com>
2020-09-04 12:32:06 -0500

    create config-overrides for react-app-rewired and add react-intl babel plugin to make the app add unique id to intl messages that do not have id

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const { override, addBabelPlugins } = require('customize-cra')
+
+module.exports = override(
+  addBabelPlugins(
+    [
+      "react-intl",
+      {
+        "idInterpolationPattern": "[sha512:contenthash:base64:6]",
+        "extractFromFormatMessageCall": true,
+        "ast": true
+      }
+    ],
+  )
+)
