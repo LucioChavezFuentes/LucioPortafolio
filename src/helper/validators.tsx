4f266206c990b25f35bd0dabbc34098a1e426987
Lucio Chavez <luciobertinchavez@gmail.com>
2020-09-07 15:57:17 -0500

    disable line for eslint warning

--- src/helper/validators.tsx
+++ src/helper/validators.tsx
@@ -16,6 +16,7 @@ export const isEmpty = (string: string) => {
 };
 
 export const isEmail = (email: string) => {
+    // eslint-disable-next-line
     const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(email.match(regExp)){
         return true;
