868085befda39d33726fd2a077ed217e1699b8f1
Lucio Chavez <luciobertinchavez@gmail.com>
2020-10-23 16:07:28 -0500

    remove redux toolkit warnings for redux persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist'
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import { persistStore, persistReducer, 
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER } from 'redux-persist'
 //import storage from 'redux-persist/lib/storage'
 import localForage from "localforage"
 
@@ -23,7 +29,12 @@ export type AppDispatch = typeof store.dispatch
 
 
 const store = configureStore({
-    reducer: persistedReducer
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
   })
 
 export const persistor = persistStore(store)
