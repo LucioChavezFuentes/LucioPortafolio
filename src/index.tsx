bef0ba25cad71161f68acc29e989b9ee400bb3bc
Lucio Chavez <luciobertinchavez@gmail.com>
2020-10-30 17:03:42 -0600

    refactor index

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
-import { createBrowserHistory } from "history";
 
-//Router
-import { Router } from "react-router-dom";
-
-//Redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
-
-//Themes Imports
-import themes from './themes/constants';
-
-import {MuiThemeProvider} from '@material-ui/core/styles';
-
-const hist = createBrowserHistory();
+//Providers
+import AppProviders from './AppProviders';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <MuiThemeProvider theme={themes.THEME_OBJECT}>
-      <Router history={hist}>
-        <App />
-      </Router>
-    </MuiThemeProvider>
-  </Provider>,
+  <AppProviders>
+    <App />
+  </AppProviders>,
   document.getElementById("root")
 );
