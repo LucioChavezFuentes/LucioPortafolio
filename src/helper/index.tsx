bbf63ab22e3989e592935e2bf029d19543a135e8
Lucio Chavez <luciobertinchavez@gmail.com>
2021-02-13 15:58:04 -0600

    move helpers import to index

---
+++ src/helper/index.tsx
@@ -0,0 +1,9 @@
+
+import AddDrawerDarkTheme from './AddDrawerDarkTheme';
+import getDarkOrLightTheme from './getDarkOrLightTheme';
+import IntlMessage from './IntlMessages';
+import useForm from './useForm';
+import useWindowsSize from './useWindowSize';
+import {isEmail, isEmpty, validateMessageData} from './validators';
+
+export {AddDrawerDarkTheme, getDarkOrLightTheme, IntlMessage, useForm, useWindowsSize, isEmail, isEmpty, validateMessageData};
