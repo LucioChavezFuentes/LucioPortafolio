92778cfa4249804737ea7278248633e3e0a4adae
Lucio Chavez <luciobertinchavez@gmail.com>
2021-03-04 14:18:57 -0600

    set opacity transition on text, preload fonts

--- src/helper/IntlMessages.tsx
+++ src/helper/IntlMessages.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-const IntlMassage = (props) => <FormattedMessage {...props} />
+
+
+const IntlMassage = (props) => {
+
+  const [opacity, setOpacity] = React.useState(0);
+
+  React.useEffect(() => {
+    setOpacity(1)
+  }, [])
+
+  return (
+    <span style={{opacity, transition: 'opacity 1s'}}>
+      <FormattedMessage {...props}  />
+    </span>
+  )
+} 
 
 export default IntlMassage;
