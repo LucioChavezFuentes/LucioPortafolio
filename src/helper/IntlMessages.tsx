47bfc7bfe081a91a21694817f239d6a1608d8183
Lucio Chavez <luciobertinchavez@gmail.com>
2021-03-05 13:56:39 -0600

    remove opacity on mount component logic

--- src/helper/IntlMessages.tsx
+++ src/helper/IntlMessages.tsx
@@ -5,16 +5,8 @@ import { FormattedMessage } from 'react-intl';
 
 const IntlMassage = (props) => {
 
-  const [opacity, setOpacity] = React.useState(0);
-
-  React.useEffect(() => {
-    setOpacity(1)
-  }, [])
-
   return (
-    <span style={{opacity, transition: 'opacity 1s'}}>
-      <FormattedMessage {...props}  />
-    </span>
+    <FormattedMessage {...props}  />
   )
 } 
 
