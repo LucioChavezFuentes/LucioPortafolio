3c28bf894da2ad67ede3393ef02da83c86fa4699
Lucio Chavez <luciobertinchavez@gmail.com>
2021-04-03 14:42:58 -0600

    add about storage comments

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,6 +6,21 @@ import { persistStore, persistReducer,
   PERSIST,
   PURGE,
   REGISTER } from 'redux-persist'
+
+/*
+LocalStorage vs LocalForage
+
+LocalStorage API is synchronous and accepts simple key value strings.
+LocalForage leverage this simple interface with Promises to get/set values and gives the ability to store more than converted strings as data.
+
+localForage library is  recommended for web (this particular project)
+check Storage Engines for different environments: https://www.npmjs.com/package/redux-persist/v/5.3.0#storage-engines
+
+but if you don't want to spend much time about storage engines and choose whats the best for you
+and you only want simple persistence then just uncomment "import storage from 'redux-persist/lib/storage'" 
+and use 'storage' instead in persistConfig.
+*/
+
 //import storage from 'redux-persist/lib/storage'
 import localForage from "localforage"
 
@@ -30,7 +45,10 @@ export type AppDispatch = typeof store.dispatch
 
 const store = configureStore({
     reducer: persistedReducer,
+    /*getDefaultMiddleware is useful if you want to add some custom middleware, 
+    but also still want to have the default middleware added as well*/
     middleware: getDefaultMiddleware({
+      //ignores redux persist actions
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
       }
